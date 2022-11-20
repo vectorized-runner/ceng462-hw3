8cdf4398912923ac5f6a001eacfe8125b68a4325
Bartu Baş <bartubas99@gmail.com>
2022-11-20 23:32:35 +0300

    WIP: Problem setup

--- hw3_e2304129.py
+++ hw3_e2304129.py
@@ -9,17 +9,41 @@ def SolveGame(method_name, problem_file_name, player_type):
     print(state)
 
     if method_name == "Minimax":
-        minimax_nim(state)
+        return minimax_nim(state)
     elif method_name == "AlphaBeta":
-        minimax_alphabeta(state)
+        return minimax_alphabeta(state)
     else:
         print("Error: unexpected method name")
     return
 
 
 def minimax_nim(state):
+    res = minimax_nim_impl(state, True, 0, [])
+    # todo: do something with result
     return
 
+def minimax_nim_impl(state, is_max, iteration, list_until):
+    # I think I can implement this.
+    # Then go to bed.
+    # It's 22.40. Give me an hour for this.
+    # Let's make a coffee?x
+
+    iteration = iteration + 1
+    list_until.append(state)
+
+    if state == (0, 0, 0):
+        if is_max:
+            return 1, iteration, list_until
+        else:
+            return -1, iteration, list_until
+
+    if is_max:
+    # todo: handle max case
+    else:
+    # todo: handle min case
+
+    print("error: reached unexpected code")
+    return -1
 
 def minimax_alphabeta(state):
     return
